0bb4153e5bdee47b0af79efba7224a9848281bbe
Gamerpro-Kelvin <163355669+Gamerpro-Kelvin@users.noreply.github.com>
2024-12-01 21:48:32 +0100

    Day11 Problem Solution

---
+++ Day11_MaximumProductSubArray.cpp
@@ -0,0 +1,89 @@
+/*
+Given an array arr[] that contains positive and negative integers (may contain 0 as well). Find the maximum product that we can get in a subarray of arr.
+
+Note: It is guaranteed that the output fits in a 32-bit integer.
+
+Examples
+
+Input: arr[] = [-2, 6, -3, -10, 0, 2]
+Output: 180
+Explanation: The subarray with maximum product is {6, -3, -10} with product = 6 * (-3) * (-10) = 180.
+
+Input: arr[] = [-1, -3, -10, 0, 6]
+Output: 30
+Explanation: The subarray with maximum product is {-3, -10} with product = (-3) * (-10) = 30.
+
+Input: arr[] = [2, 3, 4] 
+Output: 24 
+Explanation: For an array with all positive elements, the result is product of all elements. 
+Constraints:
+1 ≤ arr.size() ≤ 10^6
+-10  ≤  arr[i]  ≤  10
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        int max_product = arr[0];
+        int min_product = arr[0];
+        int result = arr[0];
+    
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] < 0) {
+                swap(max_product, min_product);
+            }
+    
+            max_product = max(arr[i], max_product * arr[i]);
+            min_product = min(arr[i], min_product * arr[i]);
+    
+            result = max(result, max_product);
+        }
+    
+        return result;
+    }
+    
+        
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
+
