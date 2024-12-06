2d370bd5263547d53b9981e6a4dfae57e4b8f17a
Gamerpro-Kelvin <163355669+Gamerpro-Kelvin@users.noreply.github.com>
2024-12-06 20:24:30 +0100

    Day16 Problem Solution

---
+++ Day16_Anagram.cpp
@@ -0,0 +1,77 @@
+/*
+Given two strings s1 and s2 consisting of lowercase characters. The task is to check whether two given strings are an anagram of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different. For example, act and tac are an anagram of each other. Strings s1 and s2 can only contain lowercase alphabets.
+
+Note: You can assume both the strings s1 & s2 are non-empty.
+
+Examples :
+
+Input: s1 = "geeks", s2 = "kseeg"
+Output: true
+Explanation: Both the string have same characters with same frequency. So, they are anagrams.
+
+Input: s1 = "allergy", s2 = "allergic"
+Output: false
+Explanation: Characters in both the strings are not same, so they are not anagrams.
+
+Input: s1 = "g", s2 = "g"
+Output: true
+Explanation: Character in both the strings are same, so they are anagrams.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        if (s1.size() != s2.size()) {
+            return false; // Different lengths cannot be anagrams
+        }
+    
+        vector<int> freq(26, 0); // Frequency array for 26 lowercase letters
+    
+        // Count frequency for s1
+        for (char c : s1) {
+            freq[c - 'a']++;
+        }
+    
+        // Subtract frequency for s2
+        for (char c : s2) {
+            freq[c - 'a']--;
+        }
+    
+        // Check if all counts are zero
+        for (int count : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
