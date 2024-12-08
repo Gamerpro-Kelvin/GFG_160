5fe7b900fb555a686e6ff26c38e1f0ce5e948ebb
Gamerpro-Kelvin <163355669+Gamerpro-Kelvin@users.noreply.github.com>
2024-12-08 21:57:19 +0100

    Day18 Problem Solution

---
+++ Day18_KMPsearchPattern.cpp
@@ -0,0 +1,91 @@
+/*
+  Given two strings, one is a text string txt and the other is a pattern string pat. The task is to print the indexes of all the occurrences of the pattern string in the text string. Use 0-based indexing while returning the indices. 
+Note: Return an empty list in case of no occurrences of pattern.
+
+Examples :
+
+Input: txt = "abcab", pat = "ab"
+Output: [0, 3]
+Explanation: The string "ab" occurs twice in txt, one starts at index 0 and the other at index 3. 
+
+Input: txt = "abesdu", pat = "edu"
+Output: []
+Explanation: There's no substring "edu" present in txt.
+  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> search(string& pat, string& txt) {
+        int n = txt.size();
+        int m = pat.size();
+        vector<int> lps(m, 0); // Longest Prefix Suffix array
+
+        // Step 1: Compute the LPS array
+        int len = 0, i = 1;
+        while (i < m) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        vector<int> result;
+        i = 0; // index for txt
+        int j = 0; // index for pat
+        while (i < n) {
+            if (txt[i] == pat[j]) {
+                i++;
+                j++;
+            }
+
+            if (j == m) {
+                // Pattern found, store the start index
+                result.push_back(i - j);
+                j = lps[j - 1];
+            } else if (i < n && txt[i] != pat[j]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector<int> res = ob.search(pat, S);
+        if (res.size() == 0)
+            cout << "[]" << endl;
+        else {
+            for (int i : res)
+                cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
