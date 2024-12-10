0812d960a80b207207053ea78b93e5656738b654
Gamerpro-Kelvin <163355669+Gamerpro-Kelvin@users.noreply.github.com>
2024-12-10 21:36:07 +0100

    Day20 Problem Solution

---
+++ Day20_StringsRotation.cpp
@@ -0,0 +1,70 @@
+/*
+You are given two strings of equal lengths, s1 and s2. The task is to check if s2 is a rotated version of the string s1.
+
+Note: The characters in the strings are in lowercase.
+
+Examples :
+
+Input: s1 = "abcd", s2 = "cdab"
+Output: true
+Explanation: After 2 right rotations, s1 will become equal to s2.
+
+Input: s1 = "aab", s2 = "aba"
+Output: true
+Explanation: After 1 left rotation, s1 will become equal to s2.
+
+Input: s1 = "abcd", s2 = "acbd"
+Output: false
+Explanation: Strings are not rotations of each other.
+
+Constraints:
+1 <= s1.size(), s2.size() <= 10^5
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to check if two strings are rotations of each other or not.
+    bool areRotations(string &s1, string &s2) {
+    if (s1.length() != s2.length()) {
+        return false;  
+
+    }
+
+    string s1s1 = s1 + s1;
+    return s1s1.find(s2) != string::npos;
+
+
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s1;
+        string s2;
+        cin >> s1 >> s2;
+        Solution sol;
+        int result = sol.areRotations(s1, s2);
+        if (result) {
+            cout << "true" << endl;
+
+        } else {
+            cout << "false" << endl;
+        }
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
